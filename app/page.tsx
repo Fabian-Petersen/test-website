aa8305fccafe66bced2bdd5ce833152503b8f1b5
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2025-01-24 23:42:31 +0200

    Changed bg color

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="container justify-center w-full h-screen flex items-center">
+    <div className="container justify-center w-full h-screen flex items-center bg-slate-600">
       <h1 className="text-5xl">Website Temporarily Down for Maintenance...</h1>
     </div>
   );
