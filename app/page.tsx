898438a465e8a5cff70bd5fcc7760f1c35a207cb
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2025-01-24 18:01:15 +0200

    Changed heading v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="container justify-center w-full h-screen flex items-center">
-      <h1 className="text-5xl">Website for Testing...</h1>
+      <h1 className="text-5xl">Website for Testing v1...</h1>
     </div>
   );
 }
