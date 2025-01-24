6d7272f529ad5a750f55fe5e73dd91c42c2843c4
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2025-01-24 18:58:44 +0200

    Changed heading to temporarily down for maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="container justify-center w-full h-screen flex items-center">
-      <h1 className="text-5xl">Website for Testing v1...</h1>
+      <h1 className="text-5xl">Website Temporarily Down for Maintenance...</h1>
     </div>
   );
 }
