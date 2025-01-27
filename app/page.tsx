2236f2104435c76da9e196beab2019f63834948d
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2025-01-27 14:14:34 +0200

    add testimonialPage

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import * as React from "react";
 import LandingPage from "../components/home/LandingPage";
 import ContactPage from "@/components/contact/ContactPage";
 import ProjectsPage from "@/components/projects/ProjectPage";
+import TestimonialPage from "@/components/testimonials/TestimonialPage";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-start justify-center h-auto mx-auto overflow-hidden">
       <LandingPage />
       <ProjectsPage />
+      <TestimonialPage />
       <ContactPage />
     </main>
   );
